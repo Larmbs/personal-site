1c588e52fb3c0c2be205288a60dae28b50f9915a
Larmbs <liamisrael200@gmail.com>
2024-08-05 15:56:20 -0400

    .

--- scripts/three_js_home.js
+++ scripts/three_js_home.js
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 
 let camera, scene, renderer;
-let solid_cube_mesh, wire_cube_mesh, directed_light, sphere_mesh, sphere_mesh2;
+let directed_light, sphere_mesh, sphere_mesh2;
 
 init();
 
@@ -19,21 +19,6 @@ function init() {
 
     // Creating the scene
     scene = new THREE.Scene();
-
-    // Creating a solid cube
-    const geo_solid_cube = new THREE.BoxGeometry();
-    const mat_solid_cube = new THREE.MeshStandardMaterial({ color: 0xffffff });
-    solid_cube_mesh = new THREE.Mesh(geo_solid_cube, mat_solid_cube);
-    //scene.add(solid_cube_mesh);
-
-    // Creating the wire frame cube
-    const geo_wire_cube = new THREE.BoxGeometry(1.15, 1.15, 1.15);
-    const mat_wire_cube = new THREE.MeshStandardMaterial({
-        color: 0xffffff,
-        wireframe: true,
-    });
-    wire_cube_mesh = new THREE.Mesh(geo_wire_cube, mat_wire_cube);
-    //scene.add(wire_cube_mesh);
 
     const geo_sphere = new THREE.SphereGeometry(
         5,
@@ -101,27 +86,10 @@ function onWindowResize() {
 
 // Animation loop
 function animate() {
-    wire_cube_mesh.rotation.x = solid_cube_mesh.rotation.x += 0.004;
-    wire_cube_mesh.rotation.y = solid_cube_mesh.rotation.y += 0.006;
-
+    // Rotating spheres
     sphere_mesh.rotation.y += 0.0025;
     sphere_mesh2.rotation.y -= 0.0025;
     sphere_mesh2.rotation.x -= 0.0005;
 
     renderer.render(scene, camera);
 }
-
-// Scroll changing cube position
-document.addEventListener("scroll", async function() {
-    let scroll = window.scrollY;
-
-    if (scroll > 400) {
-        wire_cube_mesh.position.x = ((scroll - 400) / 256)**2;
-        solid_cube_mesh.position.x = ((scroll - 400) / 256)**2;
-    } else if (scroll > 800) {
-      wire_cube_mesh.position.x = solid_cube_mesh.position.x = 200;
-    }else {
-        wire_cube_mesh.position.x = 0;
-        solid_cube_mesh.position.x = 0;
-    }
-});
