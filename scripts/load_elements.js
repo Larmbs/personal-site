c7f23682b410594304cf48a3bc87d20ab2d4d91e
Larmbs <liamisrael200@gmail.com>
2024-08-04 22:28:48 -0400

    .

--- scripts/load_elements.js
+++ scripts/load_elements.js
@@ -1,15 +1,6 @@
 // Loads html snippets from external sources
 
 document.addEventListener("DOMContentLoaded", async function () {
-    async function load_hero() {
-        const hero = await fetch("elements/hero.html");
-        const text = await hero.text();
-
-        document.querySelectorAll("#hero").forEach((element) => {
-            element.innerHTML = text;
-        });
-    }
-
     async function load_navbar() {
         const navbar = await fetch("elements/navbar.html");
         const text = await navbar.text();
@@ -28,7 +19,6 @@ document.addEventListener("DOMContentLoaded", async function () {
         });
     }
 
-    load_hero();
     load_navbar();
     load_footer();
 });
